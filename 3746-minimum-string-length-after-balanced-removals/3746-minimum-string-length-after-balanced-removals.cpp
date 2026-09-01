b6ab7e0fd411da6976aed51e38a478e11e482103
Shreyas Jadhav <shreyasjadhav216@gmail.com>
2026-09-01 09:09:48 +0530

    Time: 0 ms (100%), Space: 14.9 MB (73.17%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int a = 0;
+        for(char ch : s){
+            if(ch == 'a'){
+                a++;
+            }
+            else{
+                a--;
+            }
+        }
+        return abs(a);
+    }
+};
