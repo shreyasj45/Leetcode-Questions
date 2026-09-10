d0e7662734ac0b093f1389c8a3815221aa7be9f4
Shreyas Jadhav <shreyasjadhav216@gmail.com>
2026-09-10 09:09:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (35.25%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> freq;
+        unordered_map<char, bool> used;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        string ans = "";
+        for (char ch : s) {
+            freq[ch]--;
+
+            if (used[ch]) {
+                continue;
+            }
+
+            while (!ans.empty() && ans.back() > ch && freq[ans.back()] > 0) {
+                used[ans.back()] = false;
+                ans.pop_back();
+            }
+            ans += ch;
+            used[ch] = true;
+        }
+        return ans;
+    }
+};
